2eed63e7f45e581c096b95bd006bea4fd0d28cf3
Sourajit Karmakar <sourajitk@statixos.com>
2023-03-15 21:55:16 -0400

    vendor: Enable Compose SystemUI
    * This is still WIP as in it compiles but barely anything is user facing
      just yet.
    
    Change-Id: Ida3b95c5bb15a1eab2d4ac8bdff2dd115d7661a8

--- config/common.mk
+++ config/common.mk
@@ -181,6 +181,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Enable Compose in SystemUI by default.
+SYSTEMUI_USE_COMPOSE ?= true
+
 # Fonts
 $(call inherit-product, vendor/aosp/config/fonts.mk)
 
