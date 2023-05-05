360dc9aa5d82109383a3641059350209a2ffe5bf
someone5678 <nemui3353@gmail.com>
2023-05-05 17:08:41 +0900

    Revert "themes: Pull extended monet themes"
    This reverts commit 28c3d93c1787e905337425cd8d4927286abef40c.

--- config/themes.mk
+++ config/themes.mk
@@ -2,6 +2,4 @@
 PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay \
     AndroidClearThemeOverlay \
-    AndroidVividThemeOverlay \
-    AndroidSnowPaintDropThemeOverlay \
-    AndroidEspressoThemeOverlay
+    AndroidVividThemeOverlay
