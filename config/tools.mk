380c7ab21f89ecc35de115fb5f1d0d9d8ac6b5e5
someone5678 <nemui3353@gmail.com>
2023-08-15 22:26:55 +0900

    vendor: Move tools product to separate makefile
    Change-Id: Ic2fd90fb9d7cd22d2aab19ad8df5eb13e94da68d

---
+++ config/tools.mk
@@ -0,0 +1,12 @@
+# Filesystems tools
+PRODUCT_PACKAGES += \
+    fsck.ntfs \
+    mkfs.ntfs \
+    mount.ntfs
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/fsck.ntfs \
+    system/bin/mkfs.ntfs \
+    system/bin/mount.ntfs \
+    system/%/libfuse-lite.so \
+    system/%/libntfs-3g.so
