387e94b16153f07dd438ac5c8c81beb4a464dd43
someone5678 <someone5678@users.noreply.github.com>
2023-09-29 06:18:08 +0900

    vendor: prebuilts: bin: Remove caches/persistent_properties after OS update
    * To avoid derps after dirty flash
    
    Change-Id: I423566ef7604c31b9d8e0b8401d59d49af9b8f42

--- prebuilt/common/bin/init.parasite.sh
+++ prebuilt/common/bin/init.parasite.sh
@@ -1,1 +1,6 @@
 #! /vendor/bin/sh
+
+# Remove settings cache and persistent_properties to avoid derps after dirty flash
+rm -rf /data/resource-cache
+rm -rf /data/system/package_cache
+rm -rf /data/property/persistent_properties
