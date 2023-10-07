a7f297dddeb915dfb4aaff952dc80ace6b53f13b
someone5678 <someone5678@users.noreply.github.com>
2023-10-08 01:13:51 +0900

    Revert "common: Switch to AOSPA's face unlock implementation"
    This reverts commit 0f3a6a55926465086e6bb64e4da23b0655a574c4.
    
    Change-Id: Ie873140a9c22506542362f13153132729e3528c6

--- config/common.mk
+++ config/common.mk
@@ -136,23 +136,6 @@ endif
 PRODUCT_PACKAGES += \
     BtHelper
 
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
-
-PRODUCT_PACKAGES += \
-    AOSPASettingsOverlay
-
-ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
-PRODUCT_PACKAGES += \
-    ParanoidSense
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face.sense_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
-endif
-
 # Enable Compose in SystemUI by default.
 SYSTEMUI_USE_COMPOSE ?= true
 
