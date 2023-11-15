3b5835b6bff2283dff6b43eee79661171ad0a075
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    vendor: Switch to AOSPA's face unlock implementation
    Change-Id: I11a13349aadf63820542674761b2216a27ed387f
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/common/apps.mk
+++ config/common/apps.mk
@@ -23,3 +23,20 @@ PRODUCT_PACKAGES += \
 # TouchGestures
 PRODUCT_PACKAGES += \
     TouchGestures
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+
+PRODUCT_PACKAGES += \
+    AOSPASettingsOverlay
+
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.sense_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
