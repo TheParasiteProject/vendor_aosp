b5c239f376756572c988ab2a4beb8dc86fab30e0
Jake Weinstein <jake@aospa.co>
2023-12-28 12:14:55 +0900

    aospa: Optimize dex2oat for cortex-a76 on cortex-a510
    A510 is not supported in mainline ART. Kryo785 is supported,
    but does not enable DotProd. Currently, A76 is the target
    that enables all the features we want.
    
    dex2oatd E 12-27 23:35:27 3642344 3642344 dex2oat.cc:222] Unexpected CPU variant for Arm64: cortex-a510.
    dex2oatd E 12-27 23:35:27 3642344 3642344 dex2oat.cc:222] Known variants that need a fix for a53 erratum 835769: default, generic, cortex-a53, cortex-a53.a57, cortex-a53.a72, cortex-a57, cortex-a72, cortex-a73.
    dex2oatd E 12-27 23:35:27 3642344 3642344 dex2oat.cc:222] Known variants that do not need a fix for a53 erratum 835769: cortex-a35, cortex-a55, cortex-a75, cortex-a76, exynos-m1, exynos-m2, exynos-m3, kryo, kryo300, kryo385, kryo785
    
    Change-Id: I496143af83179896136b78802a3f7d7bd99ad3b4
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -3,6 +3,12 @@ BOARD_USES_FULL_RECOVERY_IMAGE ?= true
 
 include vendor/aosp/config/BoardConfigKernel.mk
 
+# Dex2oat
+ifeq ($(TARGET_CPU_VARIANT),cortex-a510)
+    DEX2OAT_TARGET_CPU_VARIANT := cortex-a76
+    DEX2OAT_TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+endif
+
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 	include build/make/target/board/BoardConfigPixelCommon.mk
 	include hardware/qcom-caf/common/BoardConfigQcom.mk
