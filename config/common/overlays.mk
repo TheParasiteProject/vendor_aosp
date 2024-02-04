1fa496069c1c93c2e125dbd03c20e9190bcf266f
someone5678 <someone5678@users.noreply.github.com>
2024-02-04 11:37:24 +0900

    vendor: overlay: Build NavigationBarMode2ButtonOverlay again
    Change-Id: I42d8e6be8beec67226661b64658c48ff1bfe41f9

--- config/common/overlays.mk
+++ config/common/overlays.mk
@@ -5,6 +5,10 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/aosp/overlay/common
 
+# 2 Button navigation overlay
+PRODUCT_PACKAGES += \
+    NavigationBarMode2ButtonOverlay
+
 # Cutout control overlay
 PRODUCT_PACKAGES += \
     NoCutoutOverlay \
