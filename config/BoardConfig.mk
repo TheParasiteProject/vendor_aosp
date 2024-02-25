e4bbe578955dce7d7e6d8c60286f2a6d48bebdb2
someone5678 <someone5678@users.noreply.github.com>
2024-02-25 23:25:31 +0900

    vendor: Remove fwk-detect namespace
    * Not needed anymore in this project
    
    Change-Id: I0aaa77de8caa60ab6b87a1ed7afcc88df7913f02

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -11,10 +11,5 @@ endif
 
 include vendor/aosp/config/BoardConfigSoong.mk
 
-# Namespace for fwk-detect
-TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
-PRODUCT_SOONG_NAMESPACES += \
-    $(TARGET_FWK_DETECT_PATH)/fwk-detect
-
 # Certification
 include vendor/certification/BoardConfig.mk
