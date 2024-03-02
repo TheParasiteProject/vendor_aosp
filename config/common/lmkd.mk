ff34d62caed20369469e89e962b8c8857724036f
someone5678 <someone5678@users.noreply.github.com>
2024-03-02 16:00:37 +0900

    vendor: lmkd: Do not kill heavy tasks by default
    Change-Id: Ia138a7648c3b887a7714ebc1fe09f0077b50a719

--- config/common/lmkd.mk
+++ config/common/lmkd.mk
@@ -1,7 +1,7 @@
 # LMKD
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.config.low_ram?=false \
-	ro.lmk.kill_heaviest_task?=true \
+	ro.lmk.kill_heaviest_task?=false \
 	ro.lmk.kill_timeout_ms?=100 \
 	ro.lmk.use_minfree_levels?=true \
 	ro.lmk.critical_upgrade?=true \
