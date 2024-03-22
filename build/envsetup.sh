82b51d4a5cda4a80730662b4a8c1becee94d2b48
Yuan Si <do4suki@gmail.com>
2024-03-22 21:34:26 +0800

    kernel/build: Hide sensitive info in /proc/version
    According to the Linux kernel documentation:
    These two variables allow to override the user@host string displayed during boot and in /proc/version.
    The default value is the output of the commands whoami and hostname, respectively.
    
    Change-Id: Ib39b3ef5b54619e1978fb7cfb7c52973072326dc
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -945,3 +945,5 @@ function fixup_common_out_dir() {
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=android-build
+export KBUILD_BUILD_USER=nobody
+export KBUILD_BUILD_HOST=android-build
