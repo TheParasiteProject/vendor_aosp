7a5c7db9fb9faf62bd8be2a2ed06f27a89b0819e
LuK1337 <priv.luk@gmail.com>
2024-04-21 10:01:48 +0200

    config: Enable settings large screen optimization for tablets
    This was set to true by default prior to U QPR2.
    
    Test: Open settings and notice that dual pane layout is back
    Change-Id: Ide44ecbe7e30d6bf26d9c9358e46c71b35c66250

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -3,5 +3,9 @@ TARGET_DEVICE_IS_TABLET := true
 # Tablet extension
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
+# Settings
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.settings.large_screen_opt.enabled=true
+
 # Inherit full common PixelExperience stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -3,6 +3,10 @@ TARGET_DEVICE_IS_TABLET := true
 # Tablet extension
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
+# Settings
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.settings.large_screen_opt.enabled=true
+
 # Inherit full common PixelExperience stuff
 $(call inherit-product, vendor/aosp/config/common_full.mk)
 $(call inherit-product, vendor/aosp/config/wifionly.mk)
