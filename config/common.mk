e21c5062fb2bc0d19e7331730e905aaa7139e2a7
someone5678 <someone5678@users.noreply.github.com>
2024-04-25 23:03:25 +0900

    vendor: PRODUCT_SYSTEM_DEFAULT_PROPERTIES -> PRODUCT_SYSTEM_PROPERTIES
    Change-Id: If953ffeff37e3b49467fea7106bd3ca47b05bb9b

--- config/common.mk
+++ config/common.mk
@@ -14,13 +14,13 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
+PRODUCT_SYSTEM_PROPERTIES += ro.adb.secure=0
 else
 # Enable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+PRODUCT_SYSTEM_PROPERTIES += ro.adb.secure=1
 
 # Disable extra StrictMode features on all non-engineering builds
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
+PRODUCT_SYSTEM_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
 # Don't compile SystemUITests
