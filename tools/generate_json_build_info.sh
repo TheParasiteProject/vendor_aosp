4326cd4c3d332f9e4d84e00e838ff3d53f6c7499
someone5678 <someone5678@users.noreply.github.com>
2024-05-05 18:29:12 +0900

    vendor: Adapt ota helper scripts to this project
    * Generate MarkDown document instead of plain text file
    * Do not copy changelog to system partition
    
    Change-Id: Ia69c350eec361f86acdbbf95b8003aa66bf3f12b
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -9,12 +9,12 @@ file_path=$1
 if [[ -f $file_path ]]; then
     echo "Generating .json"
     file_name=$(basename "$file_path")
-    device_name=$(echo "$file_name" | cut -d'-' -f5)
+    device_name=$(echo "$file_name" | cut -d'_' -f2 | cut -d'-' -f1)
     buildprop=${OUT_DIR:-out}/target/product/$device_name/system/build.prop
     file_size=$(stat -c %s "$file_path")
     sha256=$(cut -d' ' -f1 "$file_path".sha256sum)
-    datetime=$(grep -w ro\\.build\\.date\\.utc "$buildprop" | cut -d= -f2)
-    version=$(grep -w ro\\.custom\\.version $buildprop | cut -d= -f2)
+    datetime=$(grep -w "ro\\.build\\.date\\.utc=.*" "$buildprop" | cut -d= -f2)
+    version=$(grep -w "ro\\.custom\\.version=.*" $buildprop | cut -d= -f2)
     link="https://sourceforge.net/projects/theparasiteproject/files/${device_name}/${file_name}/download"
     cat >"$file_path".json <<JSON
 {
@@ -31,6 +31,6 @@ if [[ -f $file_path ]]; then
   ]
 }
 JSON
-    mv "$file_path".json "${OUT_DIR:-out}"/target/product/"$device_name"/"$device_name".json
-    echo -e "${GREEN}Done generating ${YELLOW}$device_name.json${NC}"
+    mv "$file_path".json "${OUT_DIR:-out}"/target/product/"$device_name"/update.json
+    echo -e "${GREEN}Done generating update.json for $device_name"
 fi
