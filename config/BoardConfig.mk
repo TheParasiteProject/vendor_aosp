f7ac5d0bfea3f0582aafa9121865c92312f93357
someone5678 <someone5678@users.noreply.github.com>
2024-05-26 02:32:04 +0900

    Revert "Also set avb key path for recovery"
    * We now supports fully signed build
    
    This reverts commit 539981ac10ac59947e95609011e78cbd281caad5.
    
    Change-Id: I14fe0e510ae623a7a139ce48dbac63e900255fee

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -8,11 +8,6 @@ include build/make/target/board/BoardConfigPixelCommon.mk
 include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
-# Custom AVB Key
-ifeq ($(TARGET_USES_CUSTOM_AVB_KEY),true)
-include vendor/aosp/config/BoardConfigAvb.mk
-endif
-
 include vendor/aosp/config/BoardConfigSoong.mk
 
 # Certification
