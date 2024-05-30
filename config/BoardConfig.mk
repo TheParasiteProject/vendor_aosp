b6304875856719eaf5da4fd959d42d53bdc0be2c
Bruno Martins <bgcngm@gmail.com>
2024-05-30 21:15:12 +0100

    config: Indent BoardConfig makefiles
    Change-Id: I56c93a0fe64c99987ad4101e7e6ccdd6250828c4

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -4,8 +4,8 @@ BOARD_USES_FULL_RECOVERY_IMAGE ?= true
 include vendor/aosp/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include build/make/target/board/BoardConfigPixelCommon.mk
-include hardware/qcom-caf/common/BoardConfigQcom.mk
+	include build/make/target/board/BoardConfigPixelCommon.mk
+	include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
 include vendor/aosp/config/BoardConfigSoong.mk
