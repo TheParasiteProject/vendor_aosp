6335a43f014864452a9b8f047e91ad0e977f8e3b
someone5678 <someone5678@users.noreply.github.com>
2024-06-08 17:26:36 +0900

    vendor: optimisation: Move to system_ext for EGL Blobcache configuration
    Change-Id: Ib57de46f7ea73105a51dd9d514bb50b2dcdf21ae

--- config/common/optimisation.mk
+++ config/common/optimisation.mk
@@ -14,7 +14,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.build.shutdown_timeout?=0
 
 # EGL - Blobcache configuration
-PRODUCT_VENDOR_PROPERTIES += \
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.egl.blobcache.multifile=true \
     ro.egl.blobcache.multifile_limit=33554432
 
