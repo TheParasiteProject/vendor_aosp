08cc3a162502323662035639ace8253100e20340
someone5678 <someone5678@users.noreply.github.com>
2024-07-10 21:14:37 +0900

    Don't build apps for now
    Change-Id: I0612d0ce5e6b1a1b17eb1184ced5f59b3baba85e

--- config/common/apps.mk
+++ config/common/apps.mk
@@ -1,57 +1,57 @@
-# Battery Status Viewer
-PRODUCT_PACKAGES += \
-    BatteryStatsViewer
+# # Battery Status Viewer
+# PRODUCT_PACKAGES += \
+#     BatteryStatsViewer
 
-# BtHelper
-PRODUCT_PACKAGES += \
-    BtHelper
+# # BtHelper
+# PRODUCT_PACKAGES += \
+#     BtHelper
 
-# Camera
-ifneq ($(PRODUCT_NO_CAMERA),true)
-ifneq ($(TARGET_INCLUDE_APERTURE),false)
-PRODUCT_PACKAGES += \
-    Aperture
+# # Camera
+# ifneq ($(PRODUCT_NO_CAMERA),true)
+# ifneq ($(TARGET_INCLUDE_APERTURE),false)
+# PRODUCT_PACKAGES += \
+#     Aperture
 
-PRODUCT_PACKAGES += \
-    PixelConfigOverlayAperture \
-    PixelLauncherCustomOverlayAperture \
-    SystemUICustomOverlayAperture
-endif #TARGET_INCLUDE_APERTURE
-endif #PRODUCT_NO_CAMERA
+# PRODUCT_PACKAGES += \
+#     PixelConfigOverlayAperture \
+#     PixelLauncherCustomOverlayAperture \
+#     SystemUICustomOverlayAperture
+# endif #TARGET_INCLUDE_APERTURE
+# endif #PRODUCT_NO_CAMERA
 
-# SystemUI Flag configuration
-PRODUCT_PACKAGES += \
-    SystemUIFlagFlipper
+# # SystemUI Flag configuration
+# PRODUCT_PACKAGES += \
+#     SystemUIFlagFlipper
 
-# SystemUI plugins
-PRODUCT_PACKAGES += \
-    QuickAccessWallet
+# # SystemUI plugins
+# PRODUCT_PACKAGES += \
+#     QuickAccessWallet
 
-# TouchGestures
-PRODUCT_PACKAGES += \
-    TouchGestures
+# # TouchGestures
+# PRODUCT_PACKAGES += \
+#     TouchGestures
 
-# Updater
-ifneq ($(TARGET_INCLUDE_UPDATER),false)
-PRODUCT_PACKAGES += \
-    Updater
-endif #TARGET_INCLUDE_UPDATER
+# # Updater
+# ifneq ($(TARGET_INCLUDE_UPDATER),false)
+# PRODUCT_PACKAGES += \
+#     Updater
+# endif #TARGET_INCLUDE_UPDATER
 
-# Face Unlock
-ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
-TARGET_FACE_UNLOCK_SUPPORTED ?= true
+# # Face Unlock
+# ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+# TARGET_FACE_UNLOCK_SUPPORTED ?= true
 
-PRODUCT_PACKAGES += \
-    AOSPASettingsOverlay
+# PRODUCT_PACKAGES += \
+#     AOSPASettingsOverlay
 
-ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
-PRODUCT_PACKAGES += \
-    ParanoidSense
+# ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+# PRODUCT_PACKAGES += \
+#     ParanoidSense
 
-PRODUCT_SYSTEM_PROPERTIES += \
-    ro.face.sense_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+# PRODUCT_SYSTEM_PROPERTIES += \
+#     ro.face.sense_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
-endif
-endif
+# PRODUCT_COPY_FILES += \
+#     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+# endif
+# endif
